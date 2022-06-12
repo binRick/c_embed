96d4785750b3bc3d5a06dccb49442a044c343cbc
rick <rick@ricks-MacBook-Air.local>
2022-06-12 07:44:24 -0400

    refactor embed logic

--- embed/embed.c
+++ embed/embed.c
@@ -1,7 +1,77 @@
 #include "embed.h"
 
 
+args_t args = {
+  DEFAULT_INPUT_FILE,
+  DEFAULT_OUTPUT_DIR,
+  DEFAULT_MODE,
+  DEFAULT_VERBOSE,
+};
+
+
+int debug_args(){
+  fprintf(stderr,
+          ansistr(AC_BRIGHT_BLUE_BLACK AC_BOLD  "Verbose: %d")
+          ", "
+          ansistr(AC_RESETALL AC_BRIGHT_RED_BLACK "Input: %s")
+          ", "
+          ansistr(AC_RESETALL AC_BRIGHT_GREEN_BLACK "Output Directory: %s")
+          ", "
+          ansistr(AC_RESETALL AC_BRIGHT_GREEN_BLACK "Mode: %s")
+          ", "
+          "\n",
+          args.verbose, args.input_file, args.output_dir, args.mode
+          );
+
+  return(EXIT_SUCCESS);
+}
+
+
 int main(int argc, char **argv) {
   (void)argc; (void)argv;
+  if ((argc >= 2) && (strcmp(argv[1], "--test") == 0)) {
+    printf(AC_RESETALL AC_GREEN "OK\n" AC_RESETALL);
+    return(EXIT_SUCCESS);
+  }
   return(embed_main(argc, argv));
+
+  parse_args(argc, argv);
+  if ((strcmp(args.mode, "args") == 0)) {
+    return(debug_args());
+  }
 }
+
+
+int parse_args(int argc, char **argv){
+  char               identifier;
+  const char         *value;
+  cag_option_context context;
+
+  cag_option_prepare(&context, options, CAG_ARRAY_SIZE(options), argc, argv);
+  while (cag_option_fetch(&context)) {
+    identifier = cag_option_get(&context);
+    switch (identifier) {
+    case 'm':
+      value     = cag_option_get_value(&context);
+      args.mode = value;
+      break;
+    case 'i':
+      value           = cag_option_get_value(&context);
+      args.input_file = value;
+      break;
+    case 'o':
+      value           = cag_option_get_value(&context);
+      args.output_dir = value;
+      break;
+    case 'v':
+      args.verbose = true;
+      break;
+      break;
+    case 'h':
+      fprintf(stderr, AC_RESETALL AC_YELLOW AC_BOLD "Usage: parser [OPTION]\n" AC_RESETALL);
+      cag_option_print(options, CAG_ARRAY_SIZE(options), stdout);
+      exit(EXIT_SUCCESS);
+    }
+  }
+  return(EXIT_SUCCESS);
+} /* parse_args */
--- embed/embed.h
+++ embed/embed.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "ansicodes/ansicodes.h"
+#include "cargs/include/cargs.h"
 #include "embed-utils/embed-utils.h"
 #include "greatest/greatest.h"
 #include <ctype.h>
@@ -8,7 +10,45 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
+int parse_args(int, char **);
+int debug_args();
 
-#define WATCH_INTERVAL_MS    100
-#define TEST_ITERATIONS      1
+#define DEFAULT_INPUT_FILE    "Makefile"
+#define DEFAULT_OUTPUT_DIR    "embeds"
+#define DEFAULT_VERBOSE       false
+#define DEFAULT_MODE          "embed"
+////////////////////////////////////////////////////////////////////
 
+typedef struct CLI_ARGS {
+  char *input_file;
+  char *output_dir;
+  char *mode;
+  bool verbose;
+} args_t;
+
+static struct cag_option options[] = {
+  { .identifier     = 'i',
+    .access_letters = "i",
+    .access_name    = "input-file",
+    .value_name     = "INPUT",
+    .description    = "Input File Path" },
+  { .identifier     = 'o',
+    .access_letters = "o",
+    .access_name    = "output-dir",
+    .value_name     = "OUTPUT_DIR",
+    .description    = "Output Directory" },
+  { .identifier     = 'm',
+    .access_letters = "m",
+    .access_name    = "mode",
+    .value_name     = "MODE",
+    .description    = "Mode" },
+  { .identifier     = 'v',
+    .access_letters = "v",
+    .access_name    = "verbose",
+    .value_name     = NULL,
+    .description    = "Verbose Mode" },
+  { .identifier     = 'h',
+    .access_letters = "h",
+    .access_name    = "help",
+    .description    = "Shows the command help" }
+};
